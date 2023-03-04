1108fdb83b89708c82f44a419f02b20351821a7a
KazuDante89 <karlemmanueldorvil@gmail.com>
2023-03-04 12:02:35 -0500

    test ... 	modified:   arch/arm64/boot/dts/vendor/qcom/yupik.dtsi 	modified:   arch/arm64/configs/lisa_defconfig 	modified:   fs/exec.c 	modified:   net/sched/Kconfig

--- fs/exec.c
+++ fs/exec.c
@@ -82,6 +82,8 @@ static struct task_struct *fp_daemon;
 #define FP_DAEMON_PREFIX "/vendor/bin/hw/android.hardware.biometrics.fingerprint"
 #define MI_FP_DAEMON_PREFIX "/vendor/bin/hw/mfp-daemon"
 
+#define HWCOMPOSER_BIN_PREFIX "/vendor/bin/hw/vendor.qti.hardware.display.composer-service"
+
 #define ZYGOTE32_BIN "/system/bin/app_process32"
 #define ZYGOTE64_BIN "/system/bin/app_process64"
 static struct signal_struct *zygote32_sig;
@@ -1879,7 +1881,13 @@ static int __do_execve_file(int fd, struct filename *filename,
 				unlikely(!strncmp(filename->name,
 				MI_FP_DAEMON_PREFIX, strlen(MI_FP_DAEMON_PREFIX)))) {
 			fp_daemon = current;
-		}
+		}	else if (unlikely(!strncmp(filename->name,
+					   HWCOMPOSER_BIN_PREFIX,
+					   strlen(HWCOMPOSER_BIN_PREFIX)))) {
+			current->flags |= PF_PERF_CRITICAL;
+			set_cpus_allowed_ptr(current, cpu_perf_mask);
+		  }
+	  }
 	}
 
 	/* execve succeeded */
