674c453be3be16865d901efbd57fb3f6fe30c999
KazuDante89 <karlemmanueldorvil@gmail.com>
2023-03-06 16:04:07 -0500

    redo 	modified:   .circleci/CompileKernel.sh

--- .circleci/CompileKernel.sh
+++ .circleci/CompileKernel.sh
@@ -1,219 +1,9 @@
 #!/bin/bash
-# shellcheck disable=SC2154
+#
+# Compile script for QuicksilveR kernel
+# Copyright (C) 2020-2021 Adithya R.
 
- # Script For Building Android arm64 Kernel
- #
- # Copyright (c) 2018-2021 Panchajanya1999 <rsk52959@gmail.com>
- #
- # Licensed under the Apache License, Version 2.0 (the "License");
- # you may not use this file except in compliance with the License.
- # You may obtain a copy of the License at
- #
- #      http://www.apache.org/licenses/LICENSE-2.0
- #
- # Unless required by applicable law or agreed to in writing, software
- # distributed under the License is distributed on an "AS IS" BASIS,
- # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- # See the License for the specific language governing permissions and
- # limitations under the License.
- #
-
-#Kernel building script
-
-# Bail out if script fails
-set -e
-
-# Function to show an informational message
-msger()
-{
-	while getopts ":n:e:" opt
-	do
-		case "${opt}" in
-			n) printf "[*] $2 \n" ;;
-			e) printf "[×] $2 \n"; return 1 ;;
-		esac
-	done
-}
-
-cdir()
-{
-	cd "$1" 2>/dev/null || msger -e "The directory $1 doesn't exists !"
-}
-
-##------------------------------------------------------##
-##----------Basic Informations, COMPULSORY--------------##
-
-# The defult directory where the kernel should be placed
-KERNEL_DIR="$KERNEL_SRC"
-BASEDIR="$(basename "$KERNEL_DIR")"
-
-# The name of the Kernel, to name the ZIP
-ZIPNAME="Proton-$BLDV.zip"
-
-# The build version to also name the ZIP
-BLDV="R0.3-v0.0.0.0"
-
-# Build Author
-# Take care, it should be a universal and most probably, case-sensitive
-AUTHOR="KazuDante89"
-
-# Architecture
-ARCH=arm64
-
-# The name of the device for which the kernel is built
-MODEL="Xiaomi 11 Lite 5G NE"
-
-# The codename of the device
-DEVICE="lisa"
-
-# The defconfig which should be used. Get it from config.gz from
-# your device or check source
-DEFCONFIG="lisa_defconfig"
-
-# Specify compiler.
-# 'clang' or 'gcc'
-COMPILER=clang
-
-# Build modules. 0 = NO | 1 = YES
-MODULES=0
-
-# Specify linker.
-# 'ld.lld'(default)
-LINKER=ld.lld
-
-# Make Parameters
-MAKE_PARAMS="O=out ARCH=arm64 CC=$TC_DIR/bin/clang CLANG_TRIPLE=$TC_DIR/bin/aarch64-linux-gnu- LD=$TC_DIR/bin/ld.lld LLVM=1 LLVM_IAS=1 \ CROSS_COMPILE=$TC_DIR/bin/llvm-"
-
-MAKE_PARAMS1="ARCH=arm64 CC=$TC_DIR/bin/clang CLANG_TRIPLE=$TC_DIR/bin/aarch64-linux-gnu- LD=$TC_DIR/bin/ld.lld LLVM=1 LLVM_IAS=1 \ CROSS_COMPILE=$TC_DIR/bin/llvm-"
-
-# Clean source prior building. 1 is NO(default) | 0 is YES
-INCREMENTAL=1
-
-# Push ZIP to Telegram. 1 is YES | 0 is NO(default)
-PTTG=1
-if [ $PTTG = 1 ]
-then
-	# Set Telegram Chat ID
-	CHATID="$chat_id"
-fi
-
-# Generate a full DEFCONFIG prior building. 1 is YES | 0 is NO(default)
-DEF_REG=1
-
-# Files/artifacts
-kernel="$KERNEL_DIR/out/arch/arm64/boot/Image"
-dtb="$KERNEL_DIR/out/arch/arm64/boot/dts/vendor/qcom/yupik.dtb"
-dtbo="$KERNEL_DIR/out/arch/arm64/boot/dts/vendor/qcom/lisa-sm7325-overlay.dtbo"
-dtboimg="$KERNEL_DIR/out/arch/arm64/boot/dtbo.img"
-
-# Build dtbo.img (select this only if your source has support to building dtbo.img)
-# 1 is YES | 0 is NO(default)
-BUILD_DTBO=1
-if [ $BUILD_DTBO = 1 ]
-then
-	# Set this to your dtbo path.
-	# Defaults in folder out/arch/arm64/boot/dts
-	DTBO_PATH="$dtbo"
-fi
-
-# Sign the zipfile
-# 1 is YES | 0 is NO
-SIGN=1
-if [ $SIGN = 1 ]
-then
-	#Check for java
-	if ! hash java 2>/dev/null 2>&1; then
-		SIGN=0
-		msger -n "you may need to install java, if you wanna have Signing enabled"
-	else
-		SIGN=1
-	fi
-fi
-
-# Silence the compilation
-# 1 is YES(default) | 0 is NO
-SILENCE=0
-
-# Verbose build
-# 0 is Quiet(default)) | 1 is verbose | 2 gives reason for rebuilding targets
-VERBOSE=1
-
-# Debug purpose. Send logs on every successfull builds
-# 1 is YES | 0 is NO(default)
-LOG_DEBUG=1
-
-##------------------------------------------------------##
-##---------Do Not Touch Anything Beyond This------------##
-
-# Check if we are using a dedicated CI ( Continuous Integration ), and
-# set KBUILD_BUILD_VERSION and KBUILD_BUILD_HOST and CI_BRANCH
-
-## Set defaults first
-
-# shellcheck source=/etc/os-release
-export DISTRO=$(source /etc/os-release && echo "${NAME}")
-export KBUILD_BUILD_HOST=$(uname -a | awk '{print $2}')
-export CI_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-TERM=xterm
-
-## Check for CI
-if [ "$CI" ]
-then
-	if [ "$CIRCLECI" ]
-	then
-		export KBUILD_BUILD_VERSION=$CIRCLE_BUILD_NUM
-		export KBUILD_BUILD_HOST="CircleCI"
-		export CI_BRANCH=$CIRCLE_BRANCH
-	fi
-	if [ "$DRONE" ]
-	then
-		export KBUILD_BUILD_VERSION=$DRONE_BUILD_NUMBER
-		export KBUILD_BUILD_HOST=$DRONE_SYSTEM_HOST
-		export CI_BRANCH=$DRONE_BRANCH
-		export BASEDIR=$DRONE_REPO_NAME # overriding
-		export SERVER_URL="${DRONE_SYSTEM_PROTO}://${DRONE_SYSTEM_HOSTNAME}/${AUTHOR}/${BASEDIR}/${KBUILD_BUILD_VERSION}"
-	else
-		msger -n "Not presetting Build Version"
-	fi
-fi
-
-#Check Kernel Version
-KERVER=$(make kernelversion)
-
-
-# Set a commit head
-COMMIT_HEAD=$(git log --oneline -1)
-
-# Set Date
-DATE=$(TZ=Asia/Kolkata date +"%Y%m%d-%T")
-
-##------------------------------------------------------##
-
-exports()
-{
-	KBUILD_BUILD_USER=$AUTHOR
-	SUBARCH=$ARCH
-
-	if [ $COMPILER = "clang" ]
-	then
-		KBUILD_COMPILER_STRING=$("$TC_DIR"/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
-		PATH=$TC_DIR/bin/:$PATH
-	elif [ $COMPILER = "gcc" ]
-	then
-		KBUILD_COMPILER_STRING=$("$GCC64_DIR"/bin/aarch64-elf-gcc --version | head -n 1)
-		PATH=$GCC64_DIR/bin/:$GCC32_DIR/bin/:/usr/bin:$PATH
-	fi
-
-	BOT_MSG_URL="https://api.telegram.org/bot$token/sendMessage"
-	BOT_BUILD_URL="https://api.telegram.org/bot$token/sendDocument"
-	PROCS=$(nproc --all)
-
-	export KBUILD_BUILD_USER ARCH SUBARCH PATH \
-		KBUILD_COMPILER_STRING BOT_MSG_URL \
-		BOT_BUILD_URL PROCS
-}
-
-##---------------------------------------------------------##
+##----------------------------------------------------------##
 
 tg_post_msg()
 {
@@ -223,8 +13,6 @@ tg_post_msg()
 	-d text="$1"
 
 }
-
-##----------------------------------------------------------##
 
 tg_post_build()
 {
@@ -239,121 +27,75 @@ tg_post_build()
 	-F caption="$2 | *MD5 Checksum : *\`$MD5CHECK\`"
 }
 
+MODEL="Xiaomi 11 Lite 5G NE"
+DEVICE="lisa"
+ARCH=arm64
+BOT_MSG_URL="https://api.telegram.org/bot$token/sendMessage"
+BOT_BUILD_URL="https://api.telegram.org/bot$token/sendDocument"
+CI="Circle CI"
+COMMIT_HEAD=$(git log --oneline -1)
+KV=$(make $MAKE_PARAMS1 kernelversion)
+KBUILD_COMPILER_STRING=$("$TC_DIR"/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
+export PATH="$TC_DIR/bin:$PATH"
+
 ##----------------------------------------------------------##
 
-build_kernel()
-{
-	if [ $INCREMENTAL = 0 ]
-	then
-		msger -n "|| Cleaning Sources ||"
-		make $MAKE_PARAMS mrproper && rm -rf out
-	fi
+SECONDS=0 # builtin bash timer
+TC_DIR="$BASE_DIR/clang"
+AK3_DIR="$BASE_DIR/AnyKernel3"
+DEFCONFIG="lisa_defconfig"
+output="$BASE_DIR/Kernel/out"
 
-	if [ "$PTTG" = 1 ]
- 	then
-		tg_post_msg "<b>$KBUILD_BUILD_VERSION CI Build Triggered</b>%0A<b>Docker OS: </b><code>$DISTRO</code>%0A<b>Kernel Version : </b><code>$KERVER</code>%0A<b>Date : </b><code>$(TZ=Asia/Jakarta date)</code>%0A<b>Device : </b><code>$MODEL [$DEVICE]</code>%0A<b>Pipeline Host : </b><code>$KBUILD_BUILD_HOST</code>%0A<b>Host Core Count : </b><code>$PROCS</code>%0A<b>Compiler Used : </b><code>$KBUILD_COMPILER_STRING</code>%0A<b>Linker : </b><code>$LINKER</code>%0a<b>Branch : </b><code>$CI_BRANCH</code>%0A<b>Top Commit : </b><code>$COMMIT_HEAD</code>%0A<a href='$SERVER_URL'>Link</a>"
-	fi
+BLDV="R0.3-v0.0.0"
+ZIPNAME="Proton-$BLDV.zip"
 
-	make $MAKE_PARAMS1 $DEFCONFIG
-	if [ $DEF_REG = 1 ]
-	then
-		cp .config arch/arm64/configs/$DEFCONFIG
-		git add arch/arm64/configs/$DEFCONFIG
-		git commit -m "$DEFCONFIG: Regenerate
+MAKE_PARAMS="O=out ARCH=arm64 CC=clang CLANG_TRIPLE=aarch64-linux-gnu- LLVM=1 LLVM_IAS=1 \
+	CROSS_COMPILE=$TC_DIR/bin/llvm-"
 
-						This is an auto-generated commit"
-	fi
+MAKE_PARAMS1="ARCH=arm64 CC=clang CLANG_TRIPLE=aarch64-linux-gnu- LLVM=1 LLVM_IAS=1 \
+	CROSS_COMPILE=$TC_DIR/bin/llvm-"
 
-	BUILD_START=$(date +"%s")
-
-	if [ $COMPILER = "clang" ]
-	then
-		MAKE $MAKE_PARAMS
-	elif [ $COMPILER = "gcc" ]
-	then
-		MAKE+=(
-			CROSS_COMPILE_ARM32=arm-eabi- \
-			CROSS_COMPILE=aarch64-elf- \
-			AR=aarch64-elf-ar \
-			OBJDUMP=aarch64-elf-objdump \
-			STRIP=aarch64-elf-strip \
-			NM=aarch64-elf-nm \
-			OBJCOPY=aarch64-elf-objcopy \
-			LD=aarch64-elf-$LINKER
-		)
-	fi
-
-	if [ $SILENCE = "1" ]
-	then
-		MAKE+=( $MAKE_PARAMS C-s )
-	fi
-
-	msger -n "|| Started Compilation ||"
-	make -kj"$PROCS" $MAKE_PARAMS \
-		V=$VERBOSE \
-		"${MAKE[@]}" $MAKE_PARAMS 2>&1 | tee error.log
-	if [ $MODULES = "1" ]
-	then
-	    msger -n "|| Started Compiling Modules ||"
-	    make -j"$PROCS" $MAKE_PARAMS \
-		 "${MAKE[@]}" modules_prepare
-	    make -j"$PROCS" $MAKE_PARAMS \
-		 "${MAKE[@]}" $MAKE_PARAMS1 modules INSTALL_MOD_PATH="$KERNEL_DIR"/out/modules
-	    make -j"$PROCS" $MAKE_PARAMS \
-		 "${MAKE[@]}" $MAKE_PARAMS1 modules_install INSTALL_MOD_PATH="$KERNEL_DIR"/out/modules
-	    find "$KERNEL_DIR"/out/modules -type f -iname '*.ko' -exec cp {} $AK3_DIR/modules/system/lib/modules/ \;
-	fi
-
-		BUILD_END=$(date +"%s")
-		DIFF=$((BUILD_END - BUILD_START))
-
-		if [ -f "$kernel" ]
-		then
-			msger -n "|| Kernel successfully compiled ||"
-			if [ $BUILD_DTBO = 1 ]
-			then
-				msger -n "|| Building DTBO ||"
-				tg_post_msg "<code>Building DTBO..</code>"
-				python3 "$KERNEL_DIR/scripts/dtc/libufdt/mkdtboimg.py" \
-					create "$KERNEL_DIR/out/arch/arm64/boot/dtbo.img" --page_size=4096 "$KERNEL_DIR/$dtbo"
-			fi
-				gen_zip
-			else
-			if [ "$PTTG" = 1 ]
- 			then
-				tg_post_build "error.log" "*Build failed to compile after $((DIFF / 60)) minute(s) and $((DIFF % 60)) seconds*"
-			fi
-		fi
-
-}
-
-##--------------------------------------------------------------##
-
-gen_zip()
-{
-	msger -n "|| Zipping into a flashable zip ||"
-	mv $kernel $AK3_DIR/$kernel
-	mv $dtb $AK3_DIR/dtb
-	if [ $BUILD_DTBO = 1 ]
-	then
-		mv "$dtboimg" $AK3_DIR/dtbo.img
-	fi
-	cdir $AK3_DIR
-	zip -r9 "$ZIPNAME" * -x ".git" -x ".github" -x "README.md" -x "*placeholder"
-
-	if [ "$PTTG" = 1 ]
- 	then
-		tg_post_build "$ZIPNAME" "Build took : $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)"
-	fi
-	cd ..
-}
-
-exports
-build_kernel
-
-if [ $LOG_DEBUG = "1" ]
-then
-	tg_post_build "error.log" "$chat_id" "Debug Mode Logs"
+if [[ $1 = "-r" || $1 = "--regen" ]]; then
+	make $MAKE_PARAMS $DEFCONFIG
+	cp out/.config arch/arm64/configs/$DEFCONFIG
+	echo -e "\nSuccessfully regenerated defconfig at $DEFCONFIG"
+	exit
 fi
 
-##----------------*****-----------------------------##
+if [[ $1 = "-c" || $1 = "--clean" ]]; then
+	rm -rf out
+	echo "Cleaned output folder"
+fi
+
+mkdir -p out
+make $MAKE_PARAMS $DEFCONFIG
+
+echo -e "\nStarting compilation...\n"
+make -j$(nproc --all) $MAKE_PARAMS || exit $?
+make -j$(nproc --all) $MAKE_PARAMS INSTALL_MOD_PATH=modules INSTALL_MOD_STRIP=1 modules_install
+
+kernel="out/arch/arm64/boot/Image"
+dtb="out/arch/arm64/boot/dts/vendor/qcom/yupik.dtb"
+dtbo="out/arch/arm64/boot/dts/vendor/qcom/lisa-sm7325-overlay.dtbo"
+
+if [ -f "$kernel" ] && [ -f "$dtb" ] && [ -f "$dtbo" ]; then
+	echo -e "\nKernel compiled succesfully! Zipping up...\n"
+fi
+	cp $kernel $AK3_DIR
+	cp $dtb $AK3_DIR/dtb
+	python3 scripts/dtc/libfdt/mkdtboimg.py create $AK3_DIR/dtbo.img --page_size=4096 $dtbo
+	cp $(find out/modules/lib/modules/5.4* -name '*.ko') $AK3_DIR/modules/vendor/lib/modules/
+	cp out/modules/lib/modules/5.4*/modules.{alias,dep,softdep} $AK3_DIR/modules/vendor/lib/modules
+	cp out/modules/lib/modules/5.4*/modules.order $AK3_DIR/modules/vendor/lib/modules/modules.load
+	sed -i 's/\(kernel\/[^: ]*\/\)\([^: ]*\.ko\)/\/vendor\/lib\/modules\/\2/g' $AK3_DIR/modules/vendor/lib/modules/modules.dep
+	sed -i 's/.*\///g' $AK3_DIR/modules/vendor/lib/modules/modules.load
+	rm -rf out/arch/arm64/boot out/modules
+	cd $AK3_DIR
+	zip -r9 "$ZIPNAME" * -x .git README.md *placeholder
+	echo "Zip: $ZIPNAME"
+	tg_post_build "$ZIPNAME"
+	cd ..
+	rm -rf AnyKernel3
+	echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
+	exit 1
+fi
