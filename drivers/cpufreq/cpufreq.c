e36ae4a253a8c336ff3746cae692ba26c206f931
KazuDante89 <karlemmanueldorvil@gmail.com>
2023-03-07 09:31:58 -0500

    test to see if would effect the max frequency instead of min 	modified:   .circleci/build.sh 	modified:   drivers/cpufreq/cpufreq.c

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -2454,6 +2454,14 @@ static int cpufreq_set_policy(struct cpufreq_policy *policy,
 	new_data.min = freq_qos_read_value(&policy->constraints, FREQ_QOS_MIN);
 	new_data.max = freq_qos_read_value(&policy->constraints, FREQ_QOS_MAX);
 
+	   if (cpumask_test_cpu(policy->cpu, cpu_lp_mask))
+         cpufreq_verify_within_limits(&new_data, 1516800, new_data.min);
+else if (cpumask_test_cpu(policy->cpu, cpu_perf_mask))
+         cpufreq_verify_within_limits(&new_data, 1900800, new_data.min);
+else if (cpumask_test_cpu(policy->cpu, cpu_perfp_mask))
+         cpufreq_verify_within_limits(&new_data, 2208000, new_data.min);
+
+
 	pr_debug("setting new policy for CPU %u: %u - %u kHz\n",
 		 new_data.cpu, new_data.min, new_data.max);
 
