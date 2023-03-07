c521a83d72036607e14fb8da4ec0011cb226406f
KazuDante89 <karlemmanueldorvil@gmail.com>
2023-03-07 12:47:19 -0500

    Testing:
    qcom-cpufreq: Limit the rate of CPU clock-rate changes
    Without any form of rate limiting on CPU clock-rate changes, the CPU
    clock can be changed as frequently as every 50 usecs, which is not
    nearly enough time for the hardware to settle into the new rate. This
    can result in the CPU performing sub-optimally when the clock-rate is
    changed extremely frequently.
    
    To mitigate this, rate-limit CPU clock-rate updates to once every 10 ms.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    
    	modified:   .circleci/build.sh
    	modified:   .circleci/test.py
    	modified:   arch/arm64/boot/dts/vendor/qcom/yupik.dtsi
    	modified:   drivers/cpufreq/qcom-cpufreq.c

--- .circleci/test.py
+++ .circleci/test.py
@@ -7,9 +7,7 @@ import telegram
 async def main():
     bot = telegram.Bot("1736412292:AAEoUVF-GulmtA0CRTN-Jnosz0h9Za12NAE")
     async with bot:
-        await bot.send_document(document='Kernel/*.config', chat_id=-1001293242785)
-        await bot.send_document(document='Kernel/out/*.config', chat_id=-1001293242785)
-        await bot.send_document(document='AnyKernel3/*.zip', chat_id=-1001293242785)
+        await bot.send_document(document='$ZIPNAME', chat_id=-1001293242785)
 
 
 if __name__ == '__main__':
