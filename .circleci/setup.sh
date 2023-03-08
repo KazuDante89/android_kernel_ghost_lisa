a84a122eaeaf2e930d86d69c67b12d242b69c1e1
KazuDante89 <karlemmanueldorvil@gmail.com>
2023-03-08 17:16:51 -0500

    Wrong source branch 	modified:   .circleci/setup.sh

--- .circleci/setup.sh
+++ .circleci/setup.sh
@@ -26,7 +26,7 @@ echo "Downloading Kernel Source"
 mkdir $BASE_DIR/Kernel
 KERNEL_SRC="$BASE_DIR"/Kernel
 OUTPUT="$KERNEL_SRC"/out
-gsc https://github.com/KazuDante89/android_kernel_ghost_lisa.git -b Proton_R0.4 $KERNEL_SRC
+gsc https://github.com/KazuDante89/android_kernel_ghost_lisa.git -b Proton_R0.6 $KERNEL_SRC
 echo "Kernel Source Completed"
 
 echo "Cloning AnyKernel3"
