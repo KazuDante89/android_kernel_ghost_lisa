9c253a9931694d1531e1205471e8732972d257ac
KazuDante89 <karlemmanueldorvil@gmail.com>
2023-04-08 08:40:35 -0400

    Revert "msm: kgsl: Reduce latency while processing ioctls"
    This reverts commit 1488e3a9e81879b939489f694d7f6e64676f35d9.
    
    	modified:   drivers/gpu/msm/kgsl_ioctl.c

--- drivers/gpu/msm/kgsl_ioctl.c
+++ drivers/gpu/msm/kgsl_ioctl.c
@@ -4,6 +4,7 @@
  */
 
 #include <linux/pm_qos.h>
+
 #include "kgsl_device.h"
 #include "kgsl_sync.h"
 #include "adreno.h"
@@ -204,7 +205,7 @@ long kgsl_ioctl(struct file *filep, unsigned int cmd, unsigned long arg)
 	 */
 	struct pm_qos_request req = {
 		.type = PM_QOS_REQ_AFFINE_CORES,
-		.cpus_affine = ATOMIC_INIT(BIT(raw_smp_processor_id()))
+		.cpus_affine = BIT(raw_smp_processor_id())
 	};
 	long ret;
 
