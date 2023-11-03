2a6e6b56c1fcbe61af807c1c34134e38782fe55e
Karl-Emmanuel Dorvil <kazu@iMac.local>
2023-11-02 22:31:36 -0400

    opps 	modified:   .circleci/setup.sh

--- .circleci/setup.sh
+++ .circleci/setup.sh
@@ -22,7 +22,7 @@ echo "$(pwd)"
 cd ../..
 
 # Clone Kernel Source
-BRANCH="R0.8"
+BRANCH="R0.0"
 echo "Downloading Kernel Source branch $BRANCH"
 mkdir $BASE_DIR/Kernel
 KERNEL_SRC="$BASE_DIR"/Kernel
